6a6f73127f03c3f8e315091825351f491ee72a78
tolaji <39959545+Tolaji@users.noreply.github.com>
2026-01-18 00:10:18 +0100

    assembly update

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0102082d6d48114f51d1d09e46ffb5b44bd299bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d8622abed5449114ed7d9e02eaf846a52d488b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
